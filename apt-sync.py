722169ff75c98504e0c4ca251ce2a00de467da97
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2017-11-19 19:23:26 +0600

    Ola

--- apt-sync.py
+++ apt-sync.py
@@ -1,21 +1,44 @@
+#!/usr/bin/env python2
+
 import subprocess
 import os
+import glob
+import sys
 import json
 from datetime import datetime
 
-pkglist_raw = subprocess.check_output("dpkg --get-selections | awk '{print $1}'", shell=True)
-pkglist = pkglist_raw.split("\n")
-pkglist.pop()
+MACHINES_DIR = "machines" + os.sep
 
-machine = "minhaz-desktop"
-timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
+def pkglist():
+    pkglist_raw = subprocess.check_output("dpkg --get-selections | awk '{print $1}'", shell=True)
+    pkglist_ret = pkglist_raw.split("\n")
+    pkglist_ret.pop()
+    return set(pkglist_ret)
 
-try:
-    os.mkdir(machine)
-except:
-    pass
+def dump(machine_name, pkg_list):
+    if not os.path.exists(machine_name):
+        os.mkdir(machine_name)
+    timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
+    filename = machine_name + os.sep + timestamp + ".json"
+    with open(filename, "w") as file:
+        json.dump(pkg_list, file)
 
-filename = machine + os.sep + timestamp + ".json"
-with open(filename, "w") as file:
-    json.dump(pkglist, file)
+def load(filename):
+    with open(filename) as file:
+        return set(json.load(file))
+
+def diff(machine_with):
+    machine_file = MACHINES_DIR + machine_with + ".json"
+    set_a = load(machine_file)
+    set_b = pkglist()
+    
+    pkg_diff = [x for x in set_a if x not in set_b]
+    for elem in sorted(pkg_diff):
+        print elem
+    
+if __name__ == "__main__":
+    #machine_name = sys.argv[1]
+    #dump(machine_name, pkglist())
+    
+    diff("minhaz-desktop")
     
