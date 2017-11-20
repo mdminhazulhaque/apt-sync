a0e4c314af78dbc2f5689ba8458f45b49c8b8a95
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2017-11-20 18:29:36 +0600

    New files

--- apt-sync.py
+++ apt-sync.py
@@ -13,24 +13,23 @@ def pkglist():
     pkglist_raw = subprocess.check_output("dpkg --get-selections | awk '{print $1}'", shell=True)
     pkglist_ret = pkglist_raw.split("\n")
     pkglist_ret.pop()
-    return set(pkglist_ret)
+    return pkglist_ret
 
 def dump(machine_name, pkg_list):
     if not os.path.exists(machine_name):
         os.mkdir(machine_name)
-    timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
-    filename = machine_name + os.sep + timestamp + ".json"
+    filename = MACHINES_DIR + machine_name + ".json"
     with open(filename, "w") as file:
         json.dump(pkg_list, file)
 
 def load(filename):
     with open(filename) as file:
-        return set(json.load(file))
+        return json.load(file)
 
 def diff(machine_name):
     machine_file = MACHINES_DIR + machine_name + ".json"
-    set_a = load(machine_file)
-    set_b = pkglist()
+    set_a = set(load(machine_file))
+    set_b = set(pkglist())
     
     pkg_diff = [x for x in set_a if x not in set_b]
     
@@ -38,8 +37,13 @@ def diff(machine_name):
         print elem
     
 if __name__ == "__main__":
-    #machine_name = sys.argv[1]
-    #dump(machine_name, pkglist())
-    
-    diff("minhaz-laptop")
-    
+    try:
+        arg1 = sys.argv[1]
+        arg2 = sys.argv[2]
+        
+        if arg1 == "dump":
+            dump(arg2, pkglist())
+        elif arg1 == "diff":
+            diff(arg2)
+    except:
+        pass
