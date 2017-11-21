3a3103c4099c58fee7dfa4429040e06cb85c30ef
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2017-11-21 18:06:06 +0600

    Huge improvements

--- apt-sync.py
+++ apt-sync.py
@@ -8,42 +8,74 @@ import json
 from datetime import datetime
 
 MACHINES_DIR = "machines" + os.sep
+MACHINE_FILE = ".machine"
 
-def pkglist():
-    pkglist_raw = subprocess.check_output("dpkg --get-selections | awk '{print $1}'", shell=True)
-    pkglist_ret = pkglist_raw.split("\n")
-    pkglist_ret.pop()
-    return pkglist_ret
+def dump(machine_name):
+    print("Dumping to " + machine_name)
+    os.system("dpkg --get-selections | awk '{print $1}' > " + machine_name)
 
-def dump(machine_name, pkg_list):
-    if not os.path.exists(machine_name):
-        os.mkdir(machine_name)
-    filename = MACHINES_DIR + machine_name + ".json"
-    with open(filename, "w") as file:
-        json.dump(pkg_list, file)
-
-def load(filename):
-    with open(filename) as file:
-        return json.load(file)
-
-def diff(machine_name):
-    machine_file = MACHINES_DIR + machine_name + ".json"
-    set_a = set(load(machine_file))
-    set_b = set(pkglist())
+def diff(machine_this, machine_that, verbose=False):
+    dump(machine_this)
     
-    pkg_diff = [x for x in set_a if x not in set_b]
+    regexp = "| egrep \"<|>\""
+    out = subprocess.check_output(" ".join([
+            "diff",
+            machine_this,
+            machine_that,
+            regexp]),
+        shell=True).split("\n")
     
-    for elem in sorted(pkg_diff):
-        print elem
+    out.pop()
     
+    for item in out:
+        if item.startswith("<"):
+            print("+ " + item[2:])
+        else:
+            print("- " + item[2:])
+        
 if __name__ == "__main__":
+    if not os.path.exists(MACHINES_DIR):
+        os.mkdir(MACHINES_DIR)
+        
+    machine_this = None
+    
+    try:
+        with open(MACHINE_FILE) as file:
+            machine_this = file.read().strip()
+    except:
+        machine_this = raw_input("No machine name was deteced.\nMachine-Name: ")
+        with open(MACHINE_FILE, 'w') as file:
+            file.write(machine_this)
+            
     try:
         arg1 = sys.argv[1]
-        arg2 = sys.argv[2]
+        
+        if arg1 not in ["diff", "dump"]:
+            exit()
         
         if arg1 == "dump":
-            dump(arg2, pkglist())
+            dump(machine_this)
         elif arg1 == "diff":
-            diff(arg2)
+            os.chdir(MACHINES_DIR)
+            try:
+                diff(sys.argv[2])
+            except:
+                machines = []
+                for m in os.listdir("."):
+                    if m != machine_this:
+                        machines.append(m)
+                
+                if len(machines) == 1:
+                    machine_that = machines[0]
+                else:
+                    print("Select machine ID to compare\n")
+                    for id, name in enumerate(machines):
+                        print(str(id) + " " + name)
+                    print
+                    machine_id = raw_input("Choice: ")
+                    machine_that = machines[int(machine_id)]
+                
+                diff(machine_this, machine_that)
     except:
         pass
+    
